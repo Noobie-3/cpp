11142ac5ac370ed4c9b2378f2883e33d44b4a257
Jed Stone <jstone31@student.wiregrass.edu>
2022-04-13 09:20:48 -0400

    git add *

---
+++ MOd 4 Hands on/MOd 4 Hands on/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+
+	string name1 = "";
+	string name2 = "";
+
+
+	string old = "";
+
+	string* oldName = &old;
+
+
+}
