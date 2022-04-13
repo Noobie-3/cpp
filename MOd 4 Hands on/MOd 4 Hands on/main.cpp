4fb88a506d44e69d07ed2670bcd9bbc03fad77f2
Jed Stone <jstone31@student.wiregrass.edu>
2022-04-13 09:34:38 -0400

    new

--- MOd 4 Hands on/MOd 4 Hands on/main.cpp
+++ MOd 4 Hands on/MOd 4 Hands on/main.cpp
@@ -6,13 +6,32 @@ using namespace std;
 int main() {
 
 
+	
+	cout << "~~~~~~~ THE NAME SWAPPER ~~~~~~~~~"<< endl;
+	cout << "Enter a name"<< endl;
 	string name1 = "";
+	cin >> name1;
+	cout << "Enter a name";
 	string name2 = "";
+	cin >> name2;
+
+	cout << name1 << "-" << name2 << endl;
+
+
+	string* name1Point = &name1;
+	string* name2Point = &name2;
 
 
 	string old = "";
 
-	string* oldName = &old;
+	string* oldNamePoint = &old;
+
+	*oldNamePoint = name1;
+	*name1Point = name2;
+	*name2Point = *oldNamePoint;
+
+
+	cout << name1 << "-" << name2 << endl;
 
 
 }
