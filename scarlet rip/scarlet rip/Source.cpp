a07fb248655d958de883d78ebe131170fba3e84a
Jed Stone <jedstone567>
2022-04-24 12:58:32 -0700

    Go more attack functions

--- scarlet rip/scarlet rip/Source.cpp
+++ scarlet rip/scarlet rip/Source.cpp
@@ -7,25 +7,56 @@
 #include <vector>
 using namespace std;
 
-int Oattack(int &PHP, int &OPower) {
-	cout << PHP << endl;
-	PHP = (PHP - OPower);
-	cout << PHP << endl;
-	return PHP;
+int Oattack(int &UHP, int &OPower) {
+	UHP = (UHP - OPower);
+	cout << "The enemy attacks and deals " << OPower << "DAMAGE" << endl;
+
+	return UHP;
 }
 
 int Uattack(int &OHP, int &UPower) {
-	cout << OHP << endl;
 	OHP = (OHP - UPower);
-	cout << OHP << endl;
+	cout << "test"; 
 	return OHP;
+}
+int Defend(int &UHP, int OPower) {
+	UHP = (UHP - (OPower - (OPower * .75)));
+	cout << "Your hp is now " << UHP;
+	return UHP;
+}
+void BrainDrive() {
+
 }
 
 void battle(User &player, Other &other) {
 	while (player.UHP > 0 and other.OHP > 0) {
-		Uattack(other.OHP,player.UPower);
-		Oattack(player.UHP, other.OPower);
-		cout << "User Hp is " << player.UHP <<  " OtherHp is "<< other.OHP << endl;
+		cout << "what would you like to do ? \N||1=fight 2 = defend(75% less damage) or 3 = BRAIN DRIVE() +50% damage";
+		int choice;
+		cin >> choice;
+		while ((choice != 1) && (choice != 2) && (choice != 3) && (choice != 4)) {
+			cout << "INVALID CHOICE please choice again";
+			cin >> choice;
+		}
+		//attack function if attack is chosen
+		if (choice == 1) {
+			Uattack(other.OHP, player.UPower);
+			Oattack(player.UHP, other.OPower);
+			cout << "User Hp is " << player.UHP << " OtherHp is " << other.OHP << endl;
+			cout << "what would you like to do ? \N||1=fight 2 = defend(75% less damage) or 3 = BRAIN DRIVE() +50% damage";
+			cout << "HElllllo";
+		}
+		else if (choice == 2) {
+			Defend(player.UHP, other.OPower);
+		}
+		else if (choice == 3) {
+
+		}
+		else if (choice == 4) {
+
+		}
+
+
+		
 	}
 }
 
@@ -41,11 +72,9 @@ int main() {
 		vector<int> list{ 6, 5, 0, 2 };
 		srand(time(0));  // Initialize random number generator.
 			int RandomNum = (rand() % 4) + 1;
-			cout << (rand() % 4) + 1 << " ";
 			//Delcaring the difffernt others you can face
-			Other other1(1, 10);
+			Other other1(100, 10);
 			Other other2(2,20);
-		
 			Other other3(3,27);
 			Other other4(8,40);
 
@@ -59,9 +88,9 @@ int main() {
 
 
 
-		int keep = true;
+		int keep = false;
 
-		if (keep == true) {
+		if (keep == false) {
 			break;
 		}
 
